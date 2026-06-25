b0087d7fd69ca71b884a0928573d822d3b029cd3
Artem Stepuk <artem.stepuk@mapbox.com>
2026-06-25 14:09:18 +0300

    24.26.0-SNAPSHOT-06-24--13-38.git-363728a

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreSearchVersion: Version = "2.26.0-SNAPSHOT-06-09--10-28.git-61306fc"
-let mapboxCommonSDKVersion: Version = "24.26.0-SNAPSHOT-06-09--10-28.git-61306fc"
+let coreSearchVersion: Version = "2.26.0-SNAPSHOT-06-24--13-38.git-363728a"
+let mapboxCommonSDKVersion: Version = "24.26.0-SNAPSHOT-06-24--13-38.git-363728a"
 
 let package = Package(
     name: "MapboxSearch",
